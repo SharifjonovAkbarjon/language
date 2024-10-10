6984b585839a25a932878565a11e948f78f7c17a
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-10-10 22:41:40 +0500

    language

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hello</h2>
+    </>
+  )
+}
+
+export default App
